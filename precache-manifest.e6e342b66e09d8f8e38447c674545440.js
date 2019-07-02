ef4f2e9921b125b2e8b2102e2ad280b99ecbb65d
Miodrag Milovanovic <misko.199613@gmail.com>
2019-07-02 16:19:57 +0200

    Updates

---
+++ precache-manifest.e6e342b66e09d8f8e38447c674545440.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5d8559955090256a58befa91480d5c",
+    "url": "/OneFrame/index.html"
+  },
+  {
+    "revision": "cd9051d7c9aef6997954",
+    "url": "/OneFrame/static/css/main.b84393fb.chunk.css"
+  },
+  {
+    "revision": "6edc6c1ede32ebc2d8a4",
+    "url": "/OneFrame/static/js/2.c0c6cbb5.chunk.js"
+  },
+  {
+    "revision": "cd9051d7c9aef6997954",
+    "url": "/OneFrame/static/js/main.696a9d96.chunk.js"
+  },
+  {
+    "revision": "d90f2c7ed623189e9255",
+    "url": "/OneFrame/static/js/runtime~main.9d6fa1c2.js"
+  }
+]);
